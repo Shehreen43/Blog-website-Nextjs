d16edbf0bdd5e1f032a297f3c0b6b6c224f9b80c
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-12-28 16:37:29 +0500

    ...

--- my-blogs-website/src/app/lib/sanity.ts
+++ my-blogs-website/src/app/lib/sanity.ts
@@ -1,27 +1,20 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-// Use the values from the `.env.local` file
+// Initialize the Sanity client
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: false, // `false` means you'll get the latest data
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-01-01',
+  useCdn: false,
 });
- 
+
+// Configure the image URL builder
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: any) {
-    return (
-        builder.image(source)
-    )
+// Utility function to generate image URLs
+export function urlFor(source: Parameters<typeof builder.image>[0]) {
+  return builder.image(source);
 }
 
-
-
-
-
 export default client;
-
-
-
